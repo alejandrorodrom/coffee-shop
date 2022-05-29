69c73c06727e8cb9f8eaaf14dbd5d0d3083c199c
Alejandro Rodriguez Romero <alex98rodriguezromero@gmail.com>
2022-05-29 10:54:51 -0500

    add link component and header (component -> shop)

--- src/app/features/shop/shop.module.ts
+++ src/app/features/shop/shop.module.ts
@@ -4,12 +4,16 @@ import { CommonModule } from '@angular/common';
 import { ShopRoutingModule } from './shop-routing.module';
 import { ShopComponent } from './shop.component';
 import { HomeComponent } from './views/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LinkComponent } from '../../shared/components/link/link.component';
 
 
 @NgModule({
   declarations: [
     ShopComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    LinkComponent
   ],
   imports: [
     CommonModule,
