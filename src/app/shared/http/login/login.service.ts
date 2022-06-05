af46a77040daf18277060ae02b795746e832c0b3
Alejandro Rodriguez Romero <alex98rodriguezromero@gmail.com>
2022-06-05 14:14:55 -0500

    se agrega servicio login http

---
+++ src/app/shared/http/login/login.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  login(): Observable<{ token: string, user: string, email: string }> {
+    return this.http
+      .post<{ token: string, user: string, email: string }>('https://coffee-shop-backend-galaxy.herokuapp.com/auth', {
+        email: 'test@test.com',
+        password: 'test'
+      })
+      .pipe(
+        catchError(err => {
+          return throwError(err)
+        })
+      )
+  }
+}
